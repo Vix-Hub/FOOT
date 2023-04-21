e49a02fb5d527182a01e98d2e38daf7c7eda2579
VincenzoBoccia <vincenzo.boccia@live.it>
2023-04-21 19:19:04 +0200

    Fixed function name

--- PythonScripts/find_recovered_tracks.py
+++ PythonScripts/find_recovered_tracks.py
@@ -4,7 +4,7 @@
 import ROOT as r 
 import fedrarootlogon
 import numpy as np 
-from debug_libraries import FillVertexMergedTracks
+from debug_libraries import FillVertexTracks
 
 VertexFile1 = r.TFile("vertices_merged.root", "READ")
 vrec1 = VertexFile1.Get("EdbVertexRec")
@@ -15,8 +15,8 @@ vrec2 = VertexFile2.Get("EdbVertexRec")
 n_vertices1 = vrec1.eVTX.GetEntries()
 n_vertices2 = vrec2.eVTX.GetEntries()
 
-vtx_tracks_1 = FillVertexMergedTracks(vrec1)
-vtx_tracks_2 = FillVertexMergedTracks(vrec2)
+vtx_tracks_1 = FillVertexTracks(vrec1)
+vtx_tracks_2 = FillVertexTracks(vrec2)
 
 outFile = r.TFile("recovered_tracks.root", "RECREATE")
 outTup = r.TNtuple("tup", "Info if track is present in both vertex files in find_recovered_tracks.py", "s0id:s0plate:is_rec")
