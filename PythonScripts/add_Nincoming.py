53a4eff51d1d936514ee55c7eb02222bebccb159
VincenzoBoccia <vincenzo.boccia@live.it>
2023-05-04 09:38:35 +0200

    Added nS2

--- PythonScripts/add_Nincoming.py
+++ PythonScripts/add_Nincoming.py
@@ -14,15 +14,20 @@ OutFile = r.TFile(OutFileName, "RECREATE")
 vtx2 = vtx.CloneTree(0)
 
 n_outgoing = np.zeros(1, np.intc)
+n_S2 = np.zeros(1, np.intc)
 vtx2.Branch("n_outgoing", n_outgoing, "n_outgoing/I")
+vtx2.Branch("n_S2", n_S2, "n_S2/I")
 
 for i in range(vtx.GetEntries()):
     vtx.GetEntry(i)
-    n_temp = 0
+    n_temp, n_temp_s2 = 0, 0
     for j in range(vtx.n):
         if (vtx.incoming[j] == 1):
             n_temp += 1
+        if (vtx.npl[i] + vtx.plate[i] > 31):
+            n_temp_s2 += 1
     n_outgoing[0] = n_temp
+    n_S2[0] = n_temp_s2
     vtx2.Fill()
 
 OutFile.cd()
