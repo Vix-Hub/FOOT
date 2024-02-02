33e32b1654478ad96f271a0baaa452482df6edbb
VincenzoBoccia <vincenzo.boccia@live.it>
2024-02-02 16:18:23 +0100

    Changes to safely save merged file

--- PRIN22/Trento_Run/Reconstruction_Code/condor_parallel/merge_files.C
+++ PRIN22/Trento_Run/Reconstruction_Code/condor_parallel/merge_files.C
@@ -1,5 +1,6 @@
 
 TFile * outputfile;
+TString path = "/eos/user/v/viboccia/PRIN22/P018_bot/";
 
 void merge_files(){
 
@@ -8,7 +9,7 @@ void merge_files(){
  TChain ch("mtracks");
 
  for (int n=0; n<17493; n = n+200) {//17493
-    TString filename = Form("/eos/user/v/viboccia/PRIN22/P018_bot/mt.merged.temp.%i.root",n);
+    TString filename = Form("%smt.merged.temp.%i.root", path.Data(), n);
     //TString filename = Form("./mt.merged.temp.%i.root",n);
 
     if (gSystem->AccessPathName(filename.Data())){//returns False if file exists, True if it does not (yes, I find it weird too)
@@ -20,10 +21,15 @@ void merge_files(){
 
  }
 
- ch.Merge("/eos/user/v/viboccia/PRIN22/P018_bot/mt.merged.all.root");
- //outputfile->cd();
- //ch.Write();
- //outputfile->Close();
+ TFile *outputfile = new TFile(Form("%smt.merged.all.root", path.Data()), "RECREATE");
+    
+    // Make sure the TFile is open before merging
+    if (outputfile->IsOpen()) {
+        ch.Merge(outputfile);
+        outputfile->Close();
+    } else {
+        cout << "Error opening output file!" << endl;
+    }
 
 }
 
